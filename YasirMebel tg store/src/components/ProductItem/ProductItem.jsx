ed30cd05e75d3bd1cd3a58bdc69eee81c0dcb817
Asadulla Ravshanbekov <asadullaravshanbekov04@gmail.com>
2023-04-27 04:46:34 +0300

    images added

--- YasirMebel tg store/src/components/ProductItem/ProductItem.jsx
+++ YasirMebel tg store/src/components/ProductItem/ProductItem.jsx
@@ -10,7 +10,9 @@ const ProductItem = ({product, className, onAdd}) => {
 
     return (
         <div className={'product ' + className}>
-            <div className={'img'}/>
+            <div className={'img'}>
+                <img src={`/images/${product.id}im.png`} alt={product.title} />
+            </div>
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
             <div className={'price'}>
--- YasirMebel tg store/src/components/ProductList/ProductList.jsx
+++ YasirMebel tg store/src/components/ProductList/ProductList.jsx
@@ -3,17 +3,7 @@ import './ProductList.css';
 import ProductItem from "../ProductItem/ProductItem";
 import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback, useEffect} from "react";
-
-const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-]
+import { products } from '../../lists/products';
 
 const getTotalPrice = (items = []) => {
     return items.reduce((acc, item) => {
@@ -73,6 +63,7 @@ const ProductList = () => {
         <div className={'list'}>
             {products.map(item => (
                 <ProductItem
+                    key={item.id}
                     product={item}
                     onAdd={onAdd}
                     className={'item'}
